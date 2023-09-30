426c29b7fe3a02ff5380a93282670af0a9d9111e
Orla <G00411336@atu.ie>
2023-09-30 22:12:43 +0100

    started lab

---
+++ Labs/trains.py
@@ -0,0 +1,12 @@
+import csv 
+import requests
+
+from xml.dom.minidom import parseString
+
+url = "http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML"
+page = requests.get(url)
+doc = parseString(page.content)
+
+print(doc.toprettyxml(), end=' ')
+
+
