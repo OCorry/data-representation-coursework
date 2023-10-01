b107a28b91eb6e6f461daf9b323df9a41407af35
Orla <G00411336@atu.ie>
2023-10-01 19:46:12 +0100

    initial commit on trains file

--- Labs/trains.py
+++ Labs/trains.py
@@ -1,12 +1,51 @@
 import csv 
 import requests
 
+retrieveTags=['TrainStatus',
+              'TrainLatitude',
+              'TrainLongitude',
+              'TrainCode',
+              'TrainDate',
+              'PublicMessage',
+              'Direction'
+              ]
+
+
 from xml.dom.minidom import parseString
 
 url = "http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML"
 page = requests.get(url)
 doc = parseString(page.content)
 
-print(doc.toprettyxml(), end=' ')
+#checking that it outputs to the terminal 
+#print(doc.toprettyxml(), newline=' ')
+
+#Storing the url as a xml file 
+with open("url", "w") as xmlfp:
+    doc.writexml(xmlfp)
 
 
+
+objTrainPositionNodes =doc.getElementsByTagName("objTrainPositions")
+with open("train.csv", mode ='w', newline='') as train_file:
+    train_writer = csv.writer(train_file, delimiter= "\t", quotechar = '"', quoting=csv.QUOTE_MINIMAL)
+    for objTrainPositionsNode in objTrainPositionNodes:
+        traincodenode = objTrainPositionsNode.getElementsByTagName("TrainCode").item(0)
+        traincode = traincodenode.firstChild.nodeValue.strip()
+        
+        dataList = []
+        for retrieveTag in retrieveTags:
+            datanode = objTrainPositionsNode.getElementsByTagName(retrieveTag).item(0)
+            dataList.append(datanode.firstChild.nodeValue.strip())
+
+        train_writer.writerow(dataList)
+    """
+    #Modifying the program to print out the latitudes 
+    objTrainPositionNodes =doc.getElementsByTagName("objTrainPositions")
+    for objTrainPositionsNode in objTrainPositionNodes:
+        trainlatitude = objTrainPositionsNode.getElementsByTagName("TrainLatitude").item(0)
+        trainlatitude = trainlatitude.firstChild.nodeValue.strip()
+        #print(trainlatitude)
+    """
+
+    
