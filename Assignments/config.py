3f5fc4c53228357af3d663e2b85c29975fb7e55c
Orla <G00411336@atu.ie>
2023-10-29 11:56:15 +0000

    Assignment 04 with config.py moved to gitignore

--- Assignments/config.py
+++ Assignments/config.py
@@ -1,4 +1,4 @@
 #My own API Key
 config = {
-    "GitHubApiKey":"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"
+    "GitHubApiKey":"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"
 }
